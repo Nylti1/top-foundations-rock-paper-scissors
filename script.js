0921bd8ef45f37f15adf3bea06dcbeada7892397
Nylti <140741150+Nylti1@users.noreply.github.com>
2023-10-31 14:44:35 +0100

    Terminar logica del juego JS

--- script.js
+++ script.js
@@ -9,6 +9,38 @@ let computer;
 buttons.forEach((btn) => {
   btn.addEventListener('click', () => {
     player = btn.textContent;
-    console.log(player);
+    computerChoice();
+
+    playerText.textContent = `Player: ${player}`;
+    computerText.textContent = `Computer: ${computer}`;
+    resultText.textContent = `Result: ${checkWinner()}`;
   });
 });
+
+function computerChoice() {
+  const randNum = Math.floor(Math.random() * 3) + 1;
+
+  switch (randNum) {
+    case 1:
+      computer = 'ROCK';
+      break;
+    case 2:
+      computer = 'PAPER';
+      break;
+    case 3:
+      computer = 'SCISSORS';
+      break;
+  }
+}
+
+function checkWinner() {
+  if (computer == player) {
+    return 'Draw';
+  } else if (computer == 'ROCK') {
+    return player == 'PAPER' ? 'You Win' : 'You Lose';
+  } else if (computer == 'PAPER') {
+    return player == 'SCISSORS' ? 'You Win' : 'You Lose';
+  } else if (computer == 'SCISSORS') {
+    return player == 'ROCK' ? 'You Win' : 'You Lose';
+  }
+}
